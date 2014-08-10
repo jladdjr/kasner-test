446fbca36983232a6ab84f2064562d46a52a514b
Jim Ladd Jr <jladdjr@gmail.com>
2014-08-10 08:57:33 -0400

    KAS-49: Simple robot example

---
+++ kasner/lib/Simple.py
@@ -0,0 +1,19 @@
+# pylint: disable=R0201
+# pylint: disable=R0903
+
+"""
+Simple Robot Library.
+"""
+
+class Simple(object):
+    """
+    Simple Robot Library.
+    """
+
+    def print_value(self, value):
+        """
+        Prints a value.
+
+        @param value: Value to print.
+        """
+        print value
